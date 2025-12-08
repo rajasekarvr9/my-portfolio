681aa028d2f70ad15ce62c6bdfea60a40d61a4e6
veeramreddy9 <rajasekarreddy.v@gmail.com>
2025-12-08 15:07:16 -0500

    profile name correct

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/'   // 👈 Must match your GitHub repo name
-})
+  base: "/my-portfolio/"   // 👈 MUST match repo name exactly
+});
