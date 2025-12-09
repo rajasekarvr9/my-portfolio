305cd804d8f9722349287f6761bfd6d6d7a47a87
rajasekarvr9 <rajasekarvr9@gmail.com>
2025-12-09 12:35:39 -0500

    latest code changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,46 +1,47 @@
 import React from "react";
-//import "./Hero.css";
+import ProfileImage from "/profile-img.png";
 
 const Hero = () => {
   return (
     <section className="hero-container">
-    <div className="hero-left">
-      <h1 className="hero-title">Hey, I’m Rajasekhar Reddy.</h1>
-      <p className="hero-subtitle">
-      I help businesses architect and deploy intelligent, cloud-native Contact Center and IVR solutions with advanced call routing, automation, 
-      and omni-channel integration to deliver scalable and high-performance customer engagement platforms.    
-      </p>
-  
-      <div className="hero-buttons">
-        {/* Scroll to contact section */}
-        <button
-          className="hero-button"
-          onClick={() => {
-            const contactSection = document.getElementById("contact");
-            contactSection.scrollIntoView({ behavior: "smooth" });
-          }}
-        >
-          Work with Me
-        </button>
-  
-        {/* Download Resume */}
-        <a
-          href="/Rajasekhar_AvayaContactCenterEngineer_Resume.docx"
-          download
-          className="hero-button hero-download"
-        >
-          Download Resume
-        </a>
+      
+      {/* Left: Text */}
+      <div className="hero-content">
+        <h1 className="hero-title">Hey, I’m Rajasekhar Reddy.</h1>
+
+        <p className="hero-subtitle">
+        I help businesses architect and deploy intelligent, cloud-native Contact Center and IVR solutions with advanced call routing, automation, 
+        and omni-channel integration to deliver scalable and high-performance customer engagement platforms.         
+        </p>
+
+        <div className="hero-buttons">
+
+          <button
+            className="hero-button"
+            onClick={() =>
+              document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Work with Me
+          </button>
+
+          <a
+            href={`${import.meta.env.BASE_URL}Rajasekhar_AvayaContactCenterEngineer_Resume.docx`}
+            download="Rajasekhar_AvayaContactCenterEngineer_Resume.docx"
+            className="hero-button hero-download"
+          >
+            Download Resume
+          </a>
+        </div>
       </div>
-    </div>
-  
-    <div className="hero-right">
-      <img src="/profile.jpg" alt="profile" className="hero-image" />
-    </div>
-  </section>
+
+      {/* Right: Profile Image */}
+      <div className="hero-image-wrapper">
+        <img src={ProfileImage} alt="profile" className="hero-image" />
+      </div>
+
+    </section>
   );
 };
 
 export default Hero;
-
-
